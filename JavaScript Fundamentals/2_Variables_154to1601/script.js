3b2d8a204c7f55b313100b549d5d4d47fdd12999
swathiputchakayala <swathiputchakayala@gmail.com>
2023-12-04 20:37:16 +0530

    na

---
+++ JavaScript Fundamentals/2_Variables_154to1601/script.js
@@ -0,0 +1,12 @@
+// USing "Const" variable
+const city ="Hyderbad";
+console.log("This is using const");
+console.log(city);
+// USing "var" variable
+var age=20;
+console.log("This is using var");
+console.log(age);
+// USing "let" variable
+let fruits="mango";
+console.log("This is using let");
+console.log(fruits);
