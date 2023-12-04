4b6792716664547a72e03e58246f878066ccc618
swathiputchakayala <swathiputchakayala@gmail.com>
2023-12-04 20:59:42 +0530

    done till 161 lessons

---
+++ JavaScript Fundamentals/2_Variables and DataTypes_154to1601/script.js
@@ -0,0 +1,46 @@
+//Variables
+//* USing "Const" variable
+const city ="Hyderbad";
+console.log("This is using const");
+console.log(city);
+//? USing "var" variable
+var age=20;
+console.log("This is using var");
+console.log(age);
+//! USing "let" variable
+let fruits="mango";
+console.log("This is using let");
+console.log(fruits);
+
+///////////////////////////////////////////////////
+//! Data Types
+
+//? Premitive
+//string
+let a ="swathi";
+//number
+let num =20;
+//boolean
+let happy= false;
+//null
+let box=null;
+//undefined
+let name=undefined;
+//symbol
+let knownAboutSymbol= symbol("id")
+
+//?Non-Premitive
+// Object
+let user={
+    name:swathi,
+    age:24,
+    city:hyderbad,
+}
+//Array
+let numbers=[100,20,375,2545]
+//function
+ let greentea=()=>{
+    comsole.log("good morning! drink me");
+ }
+
+ 
